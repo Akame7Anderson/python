1f4666179ebe78481003de656866e9f96f5d344a
Akame7Anderson <andersonrobertopetry@gmail.com>
2022-05-26 21:35:06 -0300

    adicionando img

--- game.py
+++ game.py
@@ -2,7 +2,14 @@ import pygame
 pygame.init #iniciar aplicação
 x = 400
 y = 300
+pos_x = 300
+pos_y = 300
 velocidade = 10
+fundo = pygame.image.load('imgfundo.png')
+cachorro1 = pygame.image.load('cachorro1.png')
+cachorro2 = pygame.image.load('cachorro2.png')
+cachorro = pygame.image.load('cachorro.png')
+cachorro4 = pygame.image.load('cachorro4.png')
 
 #criando janela
 janela = pygame.display.set_mode((800,600)) #tamanho da tela
@@ -10,7 +17,7 @@ pygame.display.set_caption("Jogo em Python") #nome da tela
 
 janela_aberta = True
 while janela_aberta : #enquanto janela aberta
-    pygame.time.delay(50)
+    pygame.time.delay(10)
     for event in pygame.event.get() : #aguarda evento get
         if event.type == pygame.QUIT: #se evento get for QUIT
             janela_aberta = False #janela vai encerrar
@@ -25,9 +32,19 @@ while janela_aberta : #enquanto janela aberta
         x -= velocidade #decrementar
     if comandos[pygame.K_RIGHT]:
         x += velocidade #incrementar
+    janela.fill((0,0,0))
     pygame.draw.circle(janela,(0, 255, 0),(x,y), 15)
                                 #janela   "cor"    "local,tamanho"
+
+    janela.blit(fundo,(0,0))
+    janela.blit(cachorro1,(pos_x - 170,pos_y))
+    janela.blit(cachorro2,(pos_x  + 50,pos_y))
+    janela.blit(cachorro4,(pos_x + 300,pos_y))
+    janela.blit(cachorro,(x,y))
+
+    
     pygame.display.update()
+    
 
 
 
